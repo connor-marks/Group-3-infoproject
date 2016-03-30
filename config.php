af8253e21a271125c914e4ee1a7c7d7bd1a4b9d8
connor-marks <connor-marks@uiowa.edu>
2016-03-30 18:06:04 -0500

    Added files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// Site configuration.
+
+// Server information.
+$Proto = "http://";
+$Host = $_SERVER['SERVER_NAME'];
+$Base = "/~cnmarks/song/";
+
+// Title to use in browser title bar.
+$Title = "Songs";
+$Name = "Connor Marks";
+$Email = "cnmarks@uiowa.edu";
+
+
+// DB connection (from  mysql_db_info file).
+$DBUser = "cnmarks";
+$DBName = "db_cnmarks";
+$DBHost = "dbdev.cs.uiowa.edu";
+$DBPasswd = "-iFVGZ6L3B=V";
+?>
