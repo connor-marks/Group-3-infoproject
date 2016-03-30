f61964da6a3b915ccb606e386183be575f29eda5
connor-marks <connor-marks@uiowa.edu>
2016-03-30 17:50:55 -0500

    Update inputuser.php

--- inputuser.php
+++ inputuser.php
@@ -135,18 +135,6 @@ if (isset($_POST['submit'])) {
 </div>
 </div>
 
-<!----------------->
-<!---List users--->
-<!----------------->
-<div class="row">
-<div class="col-xs-12">
-	<h2><?php echo "Users"; ?></h2>
-</div>
-</div>
-
-<div class="row">
-<div class="col-xs-12">
-<table class="table table-hover">
 
 <!-- Titles for table -->
 <thead>
