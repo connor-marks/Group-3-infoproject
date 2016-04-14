a57e395debc650f62220504a88dea9760639127b
Dom Vacchiano <djvacchiano@gmail.com>
2016-04-13 20:29:15 -0500

    Added files via upload

---
+++ empAccount.php
@@ -0,0 +1,27 @@
+<?php
+include_once("util.php");
+    include_once("config.php");
+
+//Connect to database
+    $db = connect($DBHost, $DBUser, $DBPassword, $DBName);
+
+//Query database
+    $query = "SELECT name, email, phoneNumber, address FROM Employee";
+
+//Count returned rows
+if($query->num_rows != 0) { 
+//Put results in array
+	while($rows = $query->fetch_assoc())
+	{
+		$name = $rows['name'];
+		$email = $rows['email'];
+		$phoneNumber = $rows['phoneNumber'];
+		$address = $rows['address'];
+
+		echo "<p>Name: $name <br> email: $email</p>"
+	}
+//Display the results
+} else {
+	echo "N/A"
+}
+?>
