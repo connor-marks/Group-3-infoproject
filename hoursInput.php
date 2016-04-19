d73bca2f0c451c6ed893604d4950d8997843dfda
connor-marks <connor-marks@uiowa.edu>
2016-04-18 20:59:11 -0500

    Update hoursInput.php

--- hoursInput.php
+++ hoursInput.php
@@ -11,6 +11,10 @@
 
 	<!-- Following three lines are necessary for running Bootstrap -->
 	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.spin.js" type="text/javascript"></script>
+	<link href="stylesheets/jquery.spin.css" rel="stylesheet" type="text/css" />
+	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
