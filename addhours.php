f1d41b8e03eb4a2fc4e5f1422d3edfd6b566cf40
connor-marks <connor-marks@uiowa.edu>
2016-04-27 21:11:12 -0500

    Added files via upload

--- addhours.php
+++ addhours.php
@@ -10,14 +10,10 @@
 	</title>
 
 	<!-- Following three lines are necessary for running Bootstrap -->
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css" />
-	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
 	
-	
-	<link rel="stylesheet" type="text/css" href="/jquery.datetimepicker.css"/ >
-	<script src="/jquery.js"></script>
-	<script src="/build/jquery.datetimepicker.full.min.js"></script>
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -27,6 +23,15 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>	
+
+	<script>
+		$(function() {
+			$( "#startDatepicker" ).datepicker();
+		});
+		$(function() {
+			$( "#endDatepicker" ).datepicker();
+		});
+	</script>
 </head>
 
 <body>
@@ -51,17 +56,20 @@
 
 <!-- Job form -->
 <div class="form-group">
-	<label for="Job">Job</label>
-	<input type="Job" class="form-control" name="Job"/>
+	<label for="job">Job</label>
+	<input name="job" type="text" class="form-control" name="Job"/>
+
+	<label for="startDate">Start Date</label>
+	<input name="startDate" type="text" id="startDatepicker"><br>
 	
+	<label for="endDate">End Date</label>
+	<input name="endDate" type="text" id="endDatepicker">
 </div>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="jquery.spinner.js"></script>
-<link href="bootstrap-spinner.css" rel="stylesheet">
 <!-- input hours -->
 <div class="input-append spinner" data-trigger="spinner">
-<input type="text" value="1" data-rule="quantity">
+<label for="hours">Hours</label>
+<input name="hours" type="text" value="1" data-rule="quantity">
 <div class="add-on"> <a href="javascript:;" class="spin-up" data-spin="up"><i class="icon-sort-up"></i></a> <a href="javascript:;" class="spin-down" data-spin="down"><i class="icon-sort-down"></i></a> </div>
 
 </div>
@@ -70,51 +78,14 @@
 
 <!-- start to end date -->
 
-<div class="container">
-    <div class='col-md-5'>
-        <div class="form-group">
-            <label for="Start Date">Start Date</label>
-            <div class='input-group date' id='datetimepicker6'>
-                <input type='text' class="form-control" />
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
-        </div>
-    </div>
-    <div class='col-md-5'>
-        <div class="form-group">
-            <label for="EndDate">End date</label>
-            <div class='input-group date' id='datetimepicker7'>
-                <input type='text' class="form-control" />
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
-<script type="text/javascript">
-    $(function () {
-        $('#datetimepicker6').datetimepicker();
-        $('#datetimepicker7').datetimepicker({
-            useCurrent: false //Important! See issue #1075
-        });
-        $("#datetimepicker6").on("dp.change", function (e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-        });
-        $("#datetimepicker7").on("dp.change", function (e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-        });
-    });
-</script>
+
 
 
 </div>
 
 <button type="submit" class="btn btn-default" name="submit">Add</button>
 
-</form>
+
 
 </div>
 </div>
